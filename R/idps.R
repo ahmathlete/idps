7420e387c631190b1ca2c23af436d29f784f8592
ahmathlete <Ahmed.a.b.Homoudi@gmx.de>
2022-11-20 14:36:03 +0100

    including_fortran

---
+++ R/idps.R
@@ -0,0 +1,13 @@
+#' idps: A package for computating the notorious bar statistic.
+#'
+#' The idps package provides three categories of important functions:
+#' foo, bar and baz.
+#'
+#' @section idps functions:
+#' The idps functions ...
+#'
+#' @docType package
+#' @name idps
+#' @useDynLib idps, .registration=TRUE
+NULL
+#> NULL
---
+++ src/add_f.f90
@@ -0,0 +1,24 @@
+subroutine add_(x, y, answer)
+!
+! add 2 numbers
+!
+implicit none
+double precision x, y, answer
+
+answer = x + y
+
+end
+
+
+
+
+subroutine mul_(x, y, answer)
+!
+! multiply 2 numbers
+!
+implicit none
+double precision x, y, answer
+
+answer = x * y
+
+end
